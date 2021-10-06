cff4da386fb1309bef39879661a28583ddc226e6
heeyong222 <laru1072@gmail.com>
2021-10-06 15:08:58 +0900

    websocket 테스트용 html

---
+++ Golfzoin/src/main/resources/static/app.js
@@ -0,0 +1,52 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/ws-stomp');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/sub/chat/5', function (roomMessage) {
+        	console.log(roomMessage);
+        	console.log("#"+roomMessage.body);
+            showGreeting(roomMessage.body);//JSON.parse(roomMessage.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/pub/api/chat/message", {}, JSON.stringify({'author': $("#name").val(), 'data': 'test123', 'roomNo':'5', 'date':'2012-12-12', 'profile':'test1'}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
