c04f3dc2ea7d96aea64bc88c2d48f5a4a8c5cfec
heeyong222 <laru1072@gmail.com>
2021-10-14 18:04:03 +0900

    메인페이지 JoinDto 추가

---
+++ Golfzoin/src/main/java/com/golfzon/golfzoin/dto/MainJoinDto.java
@@ -0,0 +1,69 @@
+package com.golfzon.golfzoin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainJoinDto {
+	private String type;
+	private int roomNo;
+	private String title;
+	private String body;
+	private String place;
+	private String date;
+	private String time;
+	private String thumbnail;
+	
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public int getRoomNo() {
+		return roomNo;
+	}
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
