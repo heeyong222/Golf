a945ad7af85fb443b2042b0a8cd5c1274d44e5d5
heeyong222 <laru1072@gmail.com>
2021-10-28 22:25:27 +0900

    calendar용 Dto 추가

---
+++ Golfzoin/src/main/java/com/golfzon/golfzoin/dto/CalendarDto.java
@@ -0,0 +1,75 @@
+package com.golfzon.golfzoin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CalendarDto {
+	private int roomNo;
+	private String type;
+	private String title;
+	private String hostid;
+	private String date;
+	private String time;
+	private int nowcount;
+	private int totalcount;
+	private String thumbnail;
+	public int getRoomNo() {
+		return roomNo;
+	}
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getHostid() {
+		return hostid;
+	}
+	public void setHostid(String hostid) {
+		this.hostid = hostid;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getNowcount() {
+		return nowcount;
+	}
+	public void setNowcount(int nowcount) {
+		this.nowcount = nowcount;
+	}
+	public int getTotalcount() {
+		return totalcount;
+	}
+	public void setTotalcount(int totalcount) {
+		this.totalcount = totalcount;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	
+}
